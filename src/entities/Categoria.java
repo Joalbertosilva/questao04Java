8a09da4fe714156109390235e483358781d08c53
Joao Alberto <joao.bbb189@gmail.com>
2024-08-23 02:10:38 -0300

    Questão 4. Classe Produto e Categoria.

---
+++ src/entities/Categoria.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Categoria {
+    private String titulo;
+
+    public Categoria(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
---
+++ src/entities/Produto.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Produto {
+    private String nome;
+    private int estoque;
+    private double preco;
+    private Categoria categoria;
+
+    public Produto(String nome, int estoque, double preco, Categoria categoria){
+        this.nome = nome;
+        this.estoque = estoque;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
